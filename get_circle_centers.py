f2e12ded3e71842d988f0fff76d0319f135d233a
Alexandra Kapp <alexandra.kapp2@gmail.com>
2019-04-18 16:31:39 +0200

    adjust algorithm to be more precise (no rounding errors)

--- get_circle_centers.py
+++ get_circle_centers.py
@@ -2,8 +2,8 @@ import math
 from itertools import permutations, repeat
 import numpy as np
 
-# set radius in meters (e.g. here 10 km)
-radius = 1000
+# set radius in meters (e.g. here 5 km)
+radius = 5000
 
 # set bounding box (e.g. here Berlin)
 start_lat = 52.341823
@@ -11,24 +11,31 @@ start_long = 13.088209
 end_lat = 52.669724
 end_long = 13.760610
 
-# number of km per degree = ~111km 
+# number of km per degree = 40075km / 360 = ~111
 # (between 110.567km at the equator and 111.699km at the poles)
-# 1km in degree = 1 / 111.32km = 0.0089
+# 40075 km earths perimeter at equator
+# 1km in degree = 1 / 111.32 = 0.0089
 # 1m in degree = 0.0089 / 1000 = 0.0000089
-coef = 2*radius * 0.0000089
 
+one_meter_in_degree = 1 / 111.32 / 1000
+coef = 2*radius * one_meter_in_degree
+
+# distance between all latitudes always the same
 def get_new_lat(old_lat):
     return (old_lat + coef)
 
 # pi / 180 = 0.018
+# distance between longitudes depends on latitude
+# This script is only usable for rather small areas (e.g. a city), as always the start_lat is used to calculate new longs!
+# For larger areas a the longitude needs to be calculated according to the proper lat!
 def get_new_long(old_long):
-    return (old_long + coef / math.cos(start_lat * 0.018))
+    return (old_long + coef / math.cos(start_lat * (math.pi / 180)))
 
 # get all lats:
 first_row_lats = []
 second_row_lats = []
 current_lat1 = start_lat
-current_lat2 = start_lat + radius * 0.0000089
+current_lat2 = start_lat + radius * one_meter_in_degree
 while current_lat1 < end_lat:
     first_row_lats.append(current_lat1)
     second_row_lats.append(current_lat2)
@@ -39,7 +46,7 @@ while current_lat1 < end_lat:
 first_row_longs = []
 second_row_longs = []
 current_long1 = start_long
-current_long2 = start_long + (radius * 0.0000089) / math.cos(start_lat * 0.018)
+current_long2 = start_long + (radius * one_meter_in_degree) / math.cos(start_lat * 0.018)
 while current_long1 < end_long:
     first_row_longs.append(current_long1)
     second_row_longs.append(current_long2)
